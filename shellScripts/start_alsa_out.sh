a99be88ecb9bdf9fbf8a3ccbe3b6b36b9b9d343a
Paulo Vitor Itaborai <itaborala@pop-os.localdomain>
2019-05-28 18:08:52 -0300

    Pd control experiments - Routine 2 good Accident

--- shellScripts/start_alsa_out.sh
+++ shellScripts/start_alsa_out.sh
@@ -4,13 +4,18 @@ ALSAO_WORKSPACE=6
 
 echo "alsa_out Starting Up..."
 
+gnome-terminal --working-directory=NIME2019-AVMSV/shellScripts -- "./alsa_out_w_PCH.sh"
+
 gnome-terminal --working-directory=NIME2019-AVMSV/shellScripts -- "./alsa_out_w_flags_Pro0.sh"
 
 gnome-terminal --working-directory=NIME2019-AVMSV/shellScripts -- "./alsa_out_w_flags_Pro1.sh"
 
 gnome-terminal --working-directory=NIME2019-AVMSV/shellScripts -- "./alsa_out_w_flags_CODEC.sh"
 
+
 sleep 0.1
+
+ALSAO_PCH_WIN_ARG=$(wmctrl -l | grep 'Terminal' | sort -r | awk 'NR==4' | awk '{print $1;}')
 
 ALSAO_PRO0_WIN_ARG=$(wmctrl -l | grep 'Terminal' | sort -r | awk 'NR==3' | awk '{print $1;}')
 
@@ -20,6 +25,7 @@ ALSAO_PRO1_WIN_ARG=$(wmctrl -l | grep 'Terminal' | sort -r | awk 'NR==2' | awk '
 ALSAO_CODEC_WIN_ARG=$(wmctrl -l | grep 'Terminal' | sort -r | awk 'NR==1' | awk '{print $1;}')
 
 
+wmctrl -ir $ALSAO_PCH_WIN_ARG -t $ALSAO_WORKSPACE
 wmctrl -ir $ALSAO_PRO0_WIN_ARG -t $ALSAO_WORKSPACE
 wmctrl -ir $ALSAO_PRO1_WIN_ARG -t $ALSAO_WORKSPACE
 #wmctrl -ir $ALSAO_H5_WIN_ARG -t $ALSAO_WORKSPACE
